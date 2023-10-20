539731ab4e42b0ef4b94071892b6e91e31e5fe16
gaelekisde <147897627+gaelekisde@users.noreply.github.com>
2023-10-20 10:12:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,31 +4,32 @@ $minutes = document.getElementById('minutes'),
 $seconds = document.getElementById('seconds'),
 $finalMessage = document.querySelector('.final-sms');
 
-//Fecha a futuro
-const countdownDate = new Date('7 21, 2024 11:11:11').getTime();
+// Fecha a futuro
+const countdownDate = new Date('2024-07-21T11:11:11').getTime();
 
 let interval = setInterval(function(){
-    //Obtener fecha actual y milisegundos
+    // Obtener fecha actual y milisegundos
     const now = new Date().getTime();
 
-    //Obtener las distancias entre ambas fechas
+    // Obtener las distancias entre ambas fechas
     let distance = countdownDate - now;
 
-    //Calculos a dias-horas-minutos-segundos
+    // Cálculos a días-horas-minutos-segundos
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
 
-    //Escribimos resultados
+    // Escribimos resultados
     $days.innerHTML = days;
     $hours.innerHTML = hours;
     $minutes.innerHTML = minutes;
     $seconds.innerHTML = ('1' + seconds).slice(-2);
 
-    //Cuando llegue a 0
+    // Cuando llegue a 0
     if(distance < 0){
         clearInterval(interval);
         $finalMessage.style.transform = 'translateY(0)';
     }
 }, 1000);
+
