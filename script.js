791404c045741b749f12ba48bfa4fb8224147af8
gaelekisde <147897627+gaelekisde@users.noreply.github.com>
2023-10-20 11:04:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let interval = setInterval(function(){
     $days.innerHTML = days;
     $hours.innerHTML = hours;
     $minutes.innerHTML = minutes;
-    $seconds.innerHTML = String(seconds).padStart(3, '0');
+    $seconds.innerHTML = String(seconds).padStart(2, '0');
 
 
     // Cuando llegue a 0
