67b0a988b74616b593f8dde2e60d3ee5c1d13ca5
gaelekisde <gael1411hernandez@gmail.com>
2024-02-04 15:24:22 -0600

    cambio 5

--- database/db.js
+++ database/db.js
@@ -2,11 +2,13 @@ const mysql = require('mysql');
 
 const conexion = mysql.createConnection({
     host: 'localhost',
-    user: 'id19904843_root',
-    password: 'Guts321$$',
-    database: 'id19904843_cbtis'
+    user: 'process.env.DB_USER',
+    password: 'process.env.DB_PW',
+    database: 'process.env.DB_NAME'
 })
-
+//   process.env.DB_NAME,
+// process.env.DB_USER,
+// process.env.DB_PW,
 conexion.connect((error)=>{
     if(error){
         console.log("el error es"+ error)
