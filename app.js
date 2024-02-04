62a5e59a052d1928f91e04bedd1c74c2a33210ba
gaelekisde <gael1411hernandez@gmail.com>
2024-02-04 14:20:52 -0600

    cambio de puerto

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Asegúrate de usar la ruta adecuada para el enrutador
 app.use('/', require('./router'));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
